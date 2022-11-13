91f79eb9bf478ba66cd3785e919a64501bb3728c
James Kibii <32077911+edonnardkibii@users.noreply.github.com>
2022-11-13 10:44:00 +0100

    twitter config removed

--- twitterconfig.py
+++ twitterconfig.py
@@ -1,9 +1,9 @@
 api = {
-    "api key": "prhb3TEBeTe2cDbPaxlsZoVyh",
-    "api key secret": "B2jmloB8bqqMby09oN3KnCZZSfVG3HXEA2wZoJOlTxOKZwyKND"
+    "api key": "",
+    "api key secret": ""
 }
 
 access = {
-    "access token": "1522038408687747072-jKZyEwdTQ3SO5Ph6Alu1vsGJ61EDbf",
-    "access token secret": "vtXTcKyScTSrylshyi0U0L37SCxz5ww0pxR0DpR1F31Yn"
+    "access token": "",
+    "access token secret": ""
 }
